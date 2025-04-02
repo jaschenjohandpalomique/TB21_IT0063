c4137ad1293c7c8f681f90e750a72dba90dd5230
jaschenjohandpalomique <155620511+jaschenjohandpalomique@users.noreply.github.com>
2025-04-02 23:51:35 +0800

    Create POKEMASTER.js

---
+++ POKEMASTER.js
@@ -0,0 +1,42 @@
+
+
+document.getElementById('registerButton').addEventListener('click', function () {
+    const name = document.getElementById('form3Example1c').value;
+    const email = document.getElementById('form3Example3c').value;
+    const password = document.getElementById('password').value;
+    const repeatPassword = document.getElementById('repeatPassword').value;
+    const errorMessage = document.getElementById('errorMessage');
+    const emailError = document.getElementById('emailError');
+
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+        emailError.style.display = 'block'; // Show email error message
+        emailError.textContent = 'Please enter a valid email address.';
+        return;
+    } else {
+        emailError.style.display = 'none'; // Hide email error message
+    }
+
+    if (password !== repeatPassword) {
+        errorMessage.style.display = 'block'; // Show password error message
+        errorMessage.textContent = 'Passwords do not match. Please try again.';
+        return;
+    } else {
+        errorMessage.style.display = 'none'; // Hide password error message
+    }
+
+    const generateId = () => {
+        const randomSegment = () => Math.floor(1000 + Math.random() * 9000).toString();
+        return `${randomSegment()} ${randomSegment()} ${randomSegment()}`;
+    };
+
+    const userId = generateId();
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    localStorage.setItem('userId', userId);
+
+    window.location.href = 'profile.html';
+});
